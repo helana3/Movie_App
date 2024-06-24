494c9f1cda7bebc985fe3043deeda82d73b87cfb
helana3 <you@example.com>
2024-06-24 13:49:40 +0300

    movie commit

---
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -0,0 +1,101 @@
+import 'package:dio/dio.dart';
+import 'package:movies_app/core/error/exceptions.dart';
+import 'package:movies_app/core/network/api_constance.dart';
+import 'package:movies_app/core/network/error_message_model.dart';
+import 'package:movies_app/movies/data/models/movie_details_model.dart';
+import 'package:movies_app/movies/data/models/movie_model.dart';
+import 'package:movies_app/movies/data/models/recommendation_model.dart';
+import 'package:movies_app/movies/domain/usecases/get_movie_details_usecase.dart';
+import 'package:movies_app/movies/domain/usecases/get_recommendation_usecase.dart';
+
+abstract class BaseMovieRemoteDataSource {
+  Future<List<MovieModel>> getNowPlayingMovies();
+
+  Future<List<MovieModel>> getPopularMovies();
+
+  Future<List<MovieModel>> getTopRatedMovies();
+
+  Future<MovieDetailsModel> getMovieDetails(MovieDetailsParameters parameters);
+
+  Future<List<RecommendationModel>> getRecommendation(
+      RecommendationParameters parameters);
+}
+
+class MovieRemoteDataSource extends BaseMovieRemoteDataSource {
+  @override
+  Future<List<MovieModel>> getNowPlayingMovies() async {
+    final response = await Dio().get('${AppConstant.baseUrl}/movie/now_playing?api_key=${AppConstant.apiKey}');
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from((response.data["results"] as List).map(
+        (e) => MovieModel.fromJson(e),
+      ));
+    } else {
+      throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getPopularMovies() async {
+    final response = await Dio().get("${AppConstant.baseUrl}/movie/popular?api_key=${AppConstant.apiKey}");
+
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from((response.data["results"] as List).map(
+        (e) => MovieModel.fromJson(e),
+      ));
+    } else {
+      throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getTopRatedMovies() async {
+    final response = await Dio().get("${AppConstant.baseUrl}/movie/top_rated?api_key=${AppConstant.apiKey}");
+
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from((response.data["results"] as List).map(
+        (e) => MovieModel.fromJson(e),
+      ));
+    } else {
+      throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
+  }
+
+  @override
+  Future<MovieDetailsModel> getMovieDetails(
+      MovieDetailsParameters parameters) async {
+    final response =
+        await Dio().get("${AppConstant.baseUrl}/movie/${parameters.movieId}?api_key=${AppConstant.apiKey}");
+
+    if (response.statusCode == 200) {
+      return MovieDetailsModel.fromJson(response.data);
+    } else {
+      throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
+  }
+
+  @override
+  Future<List<RecommendationModel>> getRecommendation(
+      RecommendationParameters parameters) async {
+    final response =
+        await Dio().get("${AppConstant.baseUrl}/movie/${parameters.id}/recommendations?api_key=${AppConstant.apiKey}");
+
+    if (response.statusCode == 200) {
+      return List<RecommendationModel>.from(
+          (response.data["results"] as List).map(
+        (e) => RecommendationModel.fromJson(e),
+      ));
+    } else {
+      throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
+  }
+}
---
+++ lib/movies/data/models/movie_details_model.dart
@@ -0,0 +1,42 @@
+
+import 'package:movies_app/movies/domain/entities/movie_detail.dart';
+
+
+class MovieDetailsModel extends MovieDetail {
+  const MovieDetailsModel({
+    required super.backdropPath,
+    required super.genres,
+    required super.id,
+    required super.overview,
+    required super.releaseDate,
+    required super.runtime,
+    required super.title,
+    required super.voteAverage,
+  });
+
+  factory MovieDetailsModel.fromJson(Map<String, dynamic> json) =>
+      MovieDetailsModel(
+        backdropPath: json["backdrop_path"],
+        genres: List<GenresModel>.from(
+            json["genres"].map((x) => GenresModel.fromJson(x))),
+        id: json["id"],
+        overview: json["overview"],
+        releaseDate: json["release_date"],
+        runtime: json["runtime"],
+        title: json["title"],
+        voteAverage: json["vote_average"].toDouble(),
+      );
+}
+
+
+class GenresModel extends Genres {
+  const GenresModel({
+    required super.name,
+    required super.id,
+  });
+
+  factory GenresModel.fromJson(Map<String, dynamic> json) => GenresModel(
+    id: json["id"],
+    name: json["name"],
+  );
+}
